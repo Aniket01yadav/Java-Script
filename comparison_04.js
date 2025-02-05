caba209ba3f945d92ee0d1521ff4098908d45ba9
Aniket <rajroyaniket@gmail.com>
2025-02-05 16:31:16 +0530

    Datatype summry

---
+++ comparison_04.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// "===" It means first it decide it compairable or not then do action
+
+console.log("2" === 2);
---
+++ datatypesummary.js
@@ -0,0 +1,44 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+ 
+let arr = [1,2,3,4,6,7,8,9]
+
+console.log(typeof myObj)
+console.log(typeof arr);
+console.log(typeof myFunction);
+
