a187c6bf11594df401b1aaf954ed8e9006d99a29
Aniket <rajroyaniket@gmail.com>
2025-02-07 23:09:07 +0530

    basic array operation

---
+++ 02_basics/arrays_02.js
@@ -0,0 +1,34 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("aniket"))
+console.log(Array.from("aniket"))
+console.log(Array.from({name: "aniket"})) // interesting
+
+let rank1 = 34
+let rank2 = 56
+let rank3 = 87
+
+console.log(Array.of(rank1, rank2, rank3));
+
+
+
