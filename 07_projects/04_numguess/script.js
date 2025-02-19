de990715c8f563815f85cd9337625dc94c421db4
Aniket <rajroyaniket@gmail.com>
2025-02-19 23:51:40 +0530

    Adding script in numguess game

---
+++ 07_projects/04_numguess/script.js
@@ -0,0 +1,12 @@
+let randomNumber = parseInt(Math.random() * 100 + 1);
+
+
+const form = document.querySelector("form");
+const storedItem = document.getElementById(storedItem);
+const btn = document.getElementById('btn')
+const preGuess = document.getElementById('preGuess')
+const remGuess = document.getElementById('remGuess')
+const lowOrHi = document.getElementsByClassName('lowOrHi')
+
+
+
